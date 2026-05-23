8f5f4061cf33fd1d0330fcba3df3c83bae0926f5
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-05-23 17:12:21 -0300

    Create initial models for clients and products

---
+++ internal/models/clients.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Client struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	Document string `json:"document"`
+	CreatedAt time.Time `json:"created_at"`
+}
