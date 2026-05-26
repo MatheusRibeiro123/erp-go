cfe2db8f00e389a92358be596e9ee15fe92b81ad
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-05-26 12:42:06 -0300

    Add GetByID method structure to client repository

--- internal/repositories/client_repository.go
+++ internal/repositories/client_repository.go
@@ -35,4 +35,21 @@ func (r *ClientRepository) GetAll() ([]models.Client, error) {
 		return nil, err
 	}
 	return clients, nil
+
+}
+// Função para obter um cliente por ID *ainda não esta em funcionamento, falta corrigir
+
+func (r *ClientRepository) GetByID(id int) (models.Client, error) {
+    query := "SELECT id , name , email , phone , document , created_at FROM clients WHERE id = $1"
+
+	row := r.DB.QueryRow(query, id)
+	
+	var client models.Client
+
+	err = row.Scan(&client.ID, &client.Name, &client.Email, &client.Phone, &client.Document, &client.CreatedAt)
+	
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
