fbd33aab67cbb896b4744e90624f9b4145ebb1d9
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-05-28 12:31:39 -0300

    feat: estrutura base do client_service

--- internal/services/client_service.go
+++ internal/services/client_service.go
@@ -13,3 +13,23 @@ type ClientService struct {
 func (s *ClientService) GetAll() ([]models.Client, error) {
 	return s.Repository.GetAll()
 }
+
+// serviço para obter um cliente por ID, delegando a responsabilidade para o repositório
+func (s *ClientService) GetByID(id int) (models.Client, error) {
+	return s.Repository.GetByID(id)
+}
+
+// serviço para criar um novo cliente, delegando a responsabilidade para o repositório
+func (s *ClientService) Create(client models.Client) (int, error) {
+	return s.Repository.Create(client)
+}
+
+// serviço para atualizar um cliente existente, delegando a responsabilidade para o repositório
+func (s *ClientService) Update(id int, client models.Client) error {
+	return s.Repository.Update(id, client)
+}
+
+// serviço para excluir um cliente, delegando a responsabilidade para o repositório
+func (s *ClientService) Delete(id int) error {
+	return s.Repository.Delete(id)
+}
