5e1b193338f410e901cb5a4bb598ecf6bc38d3f7
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-06-05 12:45:04 -0300

    feat: implementa endpoint GET /clients/:id

--- internal/handlers/client_handler.go
+++ internal/handlers/client_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"erp-go/internal/services"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,4 +22,25 @@ func (h *ClientHandler) GetAll(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, clients)
+}
+
+func (h *ClientHandler) GetByID(c *gin.Context) {
+
+	id := c.Param("id")
+
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	client, err := h.Service.GetByID(idInt)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, client)
 }
