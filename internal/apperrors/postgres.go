e29b0e348b0ff4a19495764c037188356c457775
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-07-01 12:44:00 -0300

    inicio da contrução da func TranslatePostgresError

--- internal/apperrors/postgres.go
+++ internal/apperrors/postgres.go
@@ -1,1 +1,21 @@
 package apperrors
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+func TranslatePostgresError(err error) error {
+	var pgErr *pgconn.PgError
+
+	if errors.As(err, &pgErr) {
+
+		switch pgErr.Code {
+
+		}
+
+	}
+
+	return err
+}
